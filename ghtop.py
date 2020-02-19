152476d2b028ba64cc2922a766436de67f5fc28f
Nat Friedman <nat@github.com>
2020-02-19 15:29:37 -0800

    Colors, commits statusbar

--- ghtop.py
+++ ghtop.py
@@ -1,10 +1,14 @@
-#!/usr/bin/python
+#!/usr/local/bin/python3
 
 import time
 import json
 import sys
 import shutil
-from urllib2 import urlopen, Request
+import urllib.request
+import enlighten
+import blessed
+
+term = blessed.Terminal()
 
 logfile = "log.txt"
 url = "https://api.github.com/events"
@@ -15,14 +19,14 @@ def get_token():
     return token
 
 def fetch_events():
-    request = Request(url)
+    request = urllib.request.Request(url)
     request.add_header('Authorization', 'token %s' % get_token())
-    response = urlopen(request)
+    response = urllib.request.urlopen(request)
     remaining_apis = int(response.headers['X-RateLimit-Remaining'])
     if remaining_apis < 1000:
-        print "WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING "
-        print "Remaining calls: " + str(remaining_apis)
-        print "WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING "
+        print("WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING ")
+        print("Remaining calls: " + str(remaining_apis))
+        print("WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING ")
     data = response.read()
 
     return json.loads(data)
@@ -36,27 +40,9 @@ def read_json_log(logfile):
     except:
         return []
 
-ansi_colors = [
-    "\033[0;31m", # red
-    "\033[0;32m", # green
-    "\033[0;33m", # yellow
-    "\033[0;34m", # blue
-    "\033[0;35m", # purple
-    "\033[0;36m", # cyan
-    "\033[0;37m", # light gray
-    "\033[1;30m", # dark gray
-    "\033[1;31m", # light red
-    "\033[1;32m", # light green
-    "\033[1;33m", # light yellow
-    "\033[1;34m", # light blue
-    "\033[1;35m", # light purple
-    "\033[1;36m", # light cyan
-    "\033[1;37m" # light white
-]
-
 printed_event_ids = {}
 
-def print_event(e):
+def print_event(e, commits_counter):
     #print ansi_colors[hash(e["type"]) % len(ansi_colors)]
 
     #print e["type"]
@@ -72,7 +58,7 @@ def print_event(e):
 
     if e["type"] == "ReleaseEvent":
         tag = e["payload"]["release"]["tag_name"]
-        print login + " released " + tag + " of " + repo
+        print(term.white_on_firebrick3(login + " released " + tag + " of " + repo))
     elif e["type"] == "PublicEvent":
         return
     elif e["type"] == "ForkEvent":
@@ -81,20 +67,15 @@ def print_event(e):
         return
     elif e["type"] == "IssueCommentEvent":
         issue = e["payload"]["issue"]
-        print login + " commented on issue #" + str(issue["number"]) + " on repo " + repo[:22] + " (\"" +  issue["title"][:50] + "...\")"
-#        print json.dumps(e, indent=2)
+        print(term.green(login + " commented on issue #" + str(issue["number"]) + " on repo " + repo[:22] + " (\"" +  issue["title"][:50] + "...\")"))
     elif e["type"] == "PushEvent":
         commits = e["payload"]["commits"]
-        return
-        sys.stdout.write(".")
-        if len(commits) > 1:
-            print login + " pushed " + str(len(commits)) + " commits to " + repo
-        elif len(commits) > 0:
-            print login + " pushed a commit to " + repo
+        for c in commits:
+            commits_counter.update()
     elif e["type"] == "CreateEvent":
         return
     elif e["type"] == "PullRequestEvent":
-        print login + " " + e["payload"]["action"] + " a pull request on repo " + repo[:20] + " (\"" +  e["payload"]["pull_request"]["title"][:50] + "...\")"
+        print(term.orange(login + " " + e["payload"]["action"] + " a pull request on repo " + repo[:20] + " (\"" +  e["payload"]["pull_request"]["title"][:50] + "...\")"))
         return
     elif e["type"] == "MemberEvent":
         return
@@ -105,12 +86,14 @@ def write_logs(events):
     f.close()
     shutil.move("tmp.log", logfile)
 
+manager = enlighten.get_manager()
+commits = manager.counter(desc='Commits', unit='commits', color='green')
+
 while True:
     events = fetch_events()
     log = read_json_log(logfile)
     combined = log + events
     write_logs(combined)
     for x in combined:
-        print_event(x)
+        print_event(x, commits)
     time.sleep(1)
-    
